fa979e91920cb3070cc8582e0c3ec22efd2299f2
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2023-12-02 01:13:48 +0400

    initial version of wedding countdown working

---
+++ app/src/main/java/com/example/marriagecountdown/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.marriagecountdown
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var editContainer: LinearLayout? = null
+    private var selectedDate: Date? = Date(124, 1, 15, 8, 0)
+    private var countdownDays: TextView? = null
+    private var countdownHours: TextView? = null
+    private var countdownMinutes: TextView? = null
+    private var countdownSeconds: TextView? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        editContainer = findViewById(R.id.edit_container)
+        countdownDays = findViewById(R.id.days)
+        countdownHours = findViewById(R.id.hours)
+        countdownMinutes = findViewById(R.id.minutes)
+        countdownSeconds = findViewById(R.id.seconds)
+        updateCountdown();
+        editContainer?.setOnClickListener{
+            showDatePickerDialog()
+        }
+    }
+
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        selectedDate?.let { calendar.time = it }
+
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year, monthOfYear, dayOfMonth ->
+                calendar.set(year, monthOfYear, dayOfMonth)
+                selectedDate = calendar.time
+                updateCountdown()
+            },
+            year,
+            month,
+            day
+        )
+        datePickerDialog.show()
+    }
+
+    private fun updateCountdown() {
+        val currentDate = Calendar.getInstance().time
+
+        selectedDate?.let { selectedDate ->
+            val timeDiff = selectedDate.time - currentDate.time
+            val seconds = timeDiff / 1000
+            val minutes = seconds / 60
+            val hours = minutes / 60
+            val days = hours / 24
+
+            countdownDays?.text = fillNumber(days)
+            countdownHours?.text = fillNumber(hours % 24)
+            countdownMinutes?.text = fillNumber(minutes % 60)
+            countdownSeconds?.text = fillNumber(seconds % 60)
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                updateCountdown()
+            }, 1000)
+        }
+    }
+
+    private fun fillNumber(num: Long): String{
+        return num.toString().padStart(2, '0')
+    }
+}
